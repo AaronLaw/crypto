3b56a64658bbfb9e81d65c28c7c90570941f5bf9
Aaron Law <aaronlaw@gmail.com>
2020-06-23 22:36:56 +0800

    Use price_difference() in main.py.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 
 
-# main #
 def price_difference(high, low):
     """A simple calculation on the increase / decrease of crypto price."""
     diff = (high - low) / low
     return diff
+
+# main #
+high = float(input('Give me the price high.'))
+low = float(input('Gime me the price low.'))
+
+diff = price_difference(high, low)
+diff_in_percentage = diff * 100
+print(diff_in_percentage)
