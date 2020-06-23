92fb012daf1a770a8481522ef3c683494696f227
Aaron Law <aaronlaw@gmail.com>
2020-06-23 23:18:36 +0800

    Rewrite in OOP style.

--- crypto/crypto.py
+++ crypto/crypto.py
@@ -1,1 +1,19 @@
 """Main module."""
+from dataclasses import dataclass
+
+@dataclass
+class Price:
+    """All about price."""
+    def __init__(self, price):
+        self.price: float
+        
+        try:
+            self.price = float(price)
+        except ValueError as e:
+            self.price = 0.00
+            print(f'Price must be floating point. Set price to {self.price}') # expect 0.00
+
+    def price_difference(self, higher_price):
+        """A simple calculation on the increase / decrease of crypto price."""
+        diff = (higher_price.price - self.price) / self.price
+        return diff
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-
-
-def price_difference(high, low):
-    """A simple calculation on the increase / decrease of crypto price."""
-    diff = (high - low) / low
-    return diff
+from crypto.crypto import Price
 
 # main #
-high = float(input('Give me the price high.'))
-low = float(input('Gime me the price low.'))
+high = input('Give me the price high.')
+low = input('Gime me the price low.')
 
-diff = price_difference(high, low)
+high_price = Price(high)
+low_price = Price(low)
+
+diff = low_price.price_difference(high_price)
 diff_in_percentage = diff * 100
 print(diff_in_percentage)
